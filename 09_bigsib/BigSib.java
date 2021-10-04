8cd71e90e25642ef2dc1b341991c6a4afd12f23c
RussellGoychayev <rgoychayev30@stuy.edu>
2021-10-04 01:29:17 -0400

    homework numero 9

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+/*
+Russell Goychayev
+APCS
+HW09: What Are BigSibs Good For?
+2021-10-04
+
+*/
+public class BigSib {
+	public static void greet(String input){
+		System.out.printf("Why, hello there, %s. How do you do? \n", input);
+	}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/*
+period 8: Russell Goychayev, Tasnim, Abdullah + ducky, Bob II
+APCS
+HW09: What Are BigSibs Good For?
+2021-10-04
+
+DISCOVERIES
+none
+UNRESOLVED QUESTIONS 
+none
+*/
+public class Greet {
+	public static void main(String[] args){
+		BigSib.greet("Russell");
+		BigSib.greet("Tasnim");
+		BigSib.greet("Abdullah");
+	}
+}
+
