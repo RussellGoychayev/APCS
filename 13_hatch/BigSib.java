8633b8c2b9e5dd57818eddbcff534a97f905333d
Russell Goychayev <73181977+RussellGoychayev@users.noreply.github.com>
2021-10-07 03:49:02 -0400

    Update BigSib.java

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,23 @@
+/*
+Squids: Russell Goychayev and Eric He, Bob and Bob II
+APCS
+HW13: Where do BigSibs Come From?
+2021-10-07
+*/
+
+public class BigSib {
+	String helloMsg;
+	BigSib(String input){
+		helloMsg = input;
+	}
+	String greet(String input){
+		return helloMsg  + " " + input ;
+	}
+}
+
+/*
+Disco
+
+QCC
+What role does main play? When do you need it? Why do you need it?
+*/
