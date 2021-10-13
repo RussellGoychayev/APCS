c6ee74220b28b55c16c3de3bb3f830d6340690b6
RussellGoychayev <rgoychayev30@stuy.edu>
2021-10-12 20:49:39 -0400

    le hw fourteen

---
+++ 14_overload/overcon/BigSib.java
@@ -0,0 +1,27 @@
+/*
+TER: Tasnim, Eric, Russell
+APCS
+HW14: Customize Your Creation
+2021-10-07
+*/
+
+
+/**
+DISCOVERIES
+UNRESOLVED QUESTIONS
+*/
+
+
+public class BigSib {
+  static String helloMsg;
+
+  public BigSib() {
+    helloMsg = "Word up";
+  }
+
+  public static String greet(String name){
+    return(helloMsg + " " + name);
+  }
+
+
+}
