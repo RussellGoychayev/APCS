ccbf8a737b705b872fd1505d47e2517d6d8bdd81
RussellGoychayev <rgoychayev30@stuy.edu>
2021-10-25 14:04:16 -0400

    started hw 24

---
+++ 24_while/Driver.java
@@ -0,0 +1,47 @@
+// Abdullah Faruque, Russell Goychayev (Team Bozos)
+// APCS
+// HW23: What Does Equality Look Like?
+// 2021-10-23
+// time spent- 1.0 hours
+
+/*
+DISCO: math.random is quite nice for probability. 
+QCC:
+*/
+
+public class Driver {
+
+  public static void main( String[] args ) {
+
+    //build Objects from blueprint specified by class Coin
+
+
+    //test default constructor
+      Coin mine = new Coin();
+      Coin yours= new Coin();
+      
+// flip until x heads come up
+// flip until y matches
+// flip until at least 65536 matches have come up 
+// num matches % DOB == 0
+int x, y, dob;
+
+public void Bozo(xNew, yNew, dobNew) {
+x = xNew;
+y = yNew;
+dob = dobNew;
+if( x==(mine.getHeadsCtr()+yours.getHeadsCtr())&&
+// abs(x head - y head) + abs(x tails - y tails)
+y= Math.abs(mine.getHeadsCtr()-yours.getHeadsCtr())&&
+3 &&
+4) {
+
+}
+}
+        /*===================TOP==========================
+
+      ====================BOTTOM======================*/
+
+  }//end main()
+
+}//end class
