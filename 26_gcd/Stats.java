e73a33e0879cffe67c82a62e4da6034026972065
RussellGoychayev <rgoychayev30@stuy.edu>
2021-10-28 22:15:33 -0400

    finished 26

---
+++ 26_gcd/Stats.java
@@ -0,0 +1,51 @@
+/**
+Team 1 Oscar Breen, Russell Goychayev, Ethan Lam
+APCS
+HW26 -- GCD Three Ways -- Recursion, while loop and hard code
+2021-10-27
+Time Spent: 1 hour
+
+DISCO:
+QCC: Why recursion over explicit? vice versa
+**/
+
+public class Stats {
+ public static int gcd(int a, int b) { //hard coded loop
+   int gcd = b;
+   while (a % gcd != 0 || b % gcd != 0) {
+     gcd--;
+   }
+   return gcd;
+ }
+
+
+ public static int gcdER(int a, int b) { //while loop
+   if (a > b) {
+     return gcdER(a - b, b);
+   } else if (a < b) {
+     return gcdER(a, b - a);
+   } else {
+     return a;
+   }
+ }
+
+
+ public static int gcdEW(int a, int b) { //Recursion loop
+   while (a != b) {
+     if (a > b) {
+       a -= b;
+     } else {
+       b -= a;
+     }
+   }
+   return a;
+ }
+
+ public static void main(String[] args) {
+  System.out.println(gcd(30, 10));
+  System.out.println(gcdER(100, 16));
+  System.out.println(gcdEW(100, 35));
+
+
+ }
+}
