0b6933f76dc15d836309ff61a31b7158a34dd63a
Russell Goychayev <73181977+RussellGoychayev@users.noreply.github.com>
2022-03-21 13:01:40 -0400

    Update Card.java

--- cobo/elevens/Activity4  Starter Code/Card.java
+++ cobo/elevens/Activity4  Starter Code/Card.java
@@ -7,91 +7,94 @@ Time Spent: 1.0 hours
 */
 
 /**
- * This class provides a convenient way to test shuffling methods.
+ * Card.java
+ *
+ * <code>Card</code> represents a playing card.
  */
-public class Shuffler {
+public class Card {
 
 	/**
-	 * The number of consecutive shuffle steps to be performed in each call
-	 * to each sorting procedure.
+	 * String value that holds the suit of the card
 	 */
-	private static final int SHUFFLE_COUNT = 3;
-
+	private String suit;
 
 	/**
-	 * Tests shuffling methods.
-	 * @param args is not used.
+	 * String value that holds the rank of the card
 	 */
-	public static void main(String[] args) {
-		System.out.println("Results of " + SHUFFLE_COUNT +
-								 " consecutive perfect shuffles:");
-		int[] values1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
-			perfectShuffle(values1);
-			System.out.print("  " + j + ":");
-			for (int k = 0; k < values1.length; k++) {
-				System.out.print(" " + values1[k]);
-			}
-			System.out.println();
-		}
-		System.out.println();
+	private String rank;
 
-		System.out.println("Results of " + SHUFFLE_COUNT +
-								 " consecutive efficient selection shuffles:");
-		int[] values2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
-			selectionShuffle(values2);
-			System.out.print("  " + j + ":");
-			for (int k = 0; k < values2.length; k++) {
-				System.out.print(" " + values2[k]);
-			}
-			System.out.println();
-		}
-		System.out.println();
+	/**
+	 * int value that holds the point value.
+	 */
+	private int pointValue;
+
+
+   /**
+	 * Creates a new <code>Card</code> instance.
+	 *
+	 * @param cardRank  a <code>String</code> value
+	 *                  containing the rank of the card
+	 * @param cardSuit  a <code>String</code> value
+	 *                  containing the suit of the card
+	 * @param cardPointValue an <code>int</code> value
+	 *                  containing the point value of the card
+	 */
+	public Card(String cardRank, String cardSuit, int cardPointValue) {
+		//initializes a new Card with the given rank, suit, and point value
+		rank = cardRank;
+		suit = cardSuit;
+		pointValue = cardPointValue;
 	}
 
 
 	/**
-	 * Apply a "perfect shuffle" to the argument.
-	 * The perfect shuffle algorithm splits the deck in half, then interleaves
-	 * the cards in one half with the cards in the other.
-	 * @param values is an array of integers simulating cards to be shuffled.
+	 * Accesses this <code>Card's</code> suit.
+	 * @return this <code>Card's</code> suit.
 	 */
-	public static void perfectShuffle(int[] values) {
-		int[] shuffled = new int[values.length];
-		int k = 0;
-		for ( int j = 0; j < ( values.length / 2 ); j++ ) {
-			shuffled[k] = values[j];
-			k += 2;
-		}
-		k = 1;
-		for ( int j = ( values.length / 2 ); j < values.length; j++ ) {
-			shuffled[k] = values[j];
-			k += 2;
-		}
-
-		for ( int i = 0; i < values.length; i++ ) {
-			values[i] = shuffled[i];
-		}
+	public String suit() {
+		return suit;
 	}
 
 	/**
-	 * Apply an "efficient selection shuffle" to the argument.
-	 * The selection shuffle algorithm conceptually maintains two sequences
-	 * of cards: the selected cards (initially empty) and the not-yet-selected
-	 * cards (initially the entire deck). It repeatedly does the following until
-	 * all cards have been selected: randomly remove a card from those not yet
-	 * selected and add it to the selected cards.
-	 * An efficient version of this algorithm makes use of arrays to avoid
-	 * searching for an as-yet-unselected card.
-	 * @param values is an array of integers simulating cards to be shuffled.
+	 * Accesses this <code>Card's</code> rank.
+	 * @return this <code>Card's</code> rank.
 	 */
-	public static void selectionShuffle(int[] values) {
-		for ( int k = values.length - 1 ; k >= 0; k-- ) {
-			int r = (int) ( Math.random() * (k+1) );
-			int temp = values[k];
-			values[k] = values[r];
-			values[r] = temp;
-		}
+	public String rank() {
+		return rank;
+	}
+
+   /**
+	 * Accesses this <code>Card's</code> point value.
+	 * @return this <code>Card's</code> point value.
+	 */
+	public int pointValue() {
+		return pointValue;
+	}
+
+	/** Compare this card with the argument.
+	 * @param otherCard the other card to compare to this
+	 * @return true if the rank, suit, and point value of this card
+	 *              are equal to those of the argument;
+	 *         false otherwise.
+	 */
+	public boolean matches(Card otherCard) {
+		return otherCard.suit().equals(this.suit())
+			&& otherCard.rank().equals(this.rank())
+			&& otherCard.pointValue() == this.pointValue();
+	}
+
+	/**
+	 * Converts the rank, suit, and point value into a string in the format
+	 *     "[Rank] of [Suit] (point value = [PointValue])".
+	 * This provides a useful way of printing the contents
+	 * of a <code>Deck</code> in an easily readable format or performing
+	 * other similar functions.
+	 *
+	 * @return a <code>String</code> containing the rank, suit,
+	 *         and point value of the card.
+	 */
+	@Override
+	public String toString() {
+		return rank + " of " + suit + " (point value = " + pointValue + ")";
 	}
 }
